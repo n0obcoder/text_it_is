3ecf3a96982a62f7bfe051e135f5bf7cc52b8c7e
n0obcoder <31921968+n0obcoder@users.noreply.github.com>
2019-01-28 20:51:35 +0530

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,5 +1,9 @@
-import os
+'''
+DATE 28th JAN 2019
+THIS CODE GETS THE reviews_list WHICH CONTAINS ALL THE REVIEWS FROM read_data.py. THESE REVIEWS HAVE TO BE PREPROCESSED BEFORE THEY CAN BE USED FOR MAKING WORD EMBEDDINGS.
+'''
 
+import os
 from read_data import read_data
 
 reviews_list = read_data()
